2c9089594832c7120ffecd45897702dcc3ac4d4d
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2025-08-22 16:21:14 -0300

    feat: solução de parte das instâncias

--- count.py
+++ count.py
@@ -5,3 +5,6 @@ pasta_atual = os.path.dirname(os.path.abspath(__file__))
 arquivos_txt = [f for f in os.listdir(pasta_atual) if f.lower().endswith('.txt')]
 
 print(f"Quantidade de arquivos .txt na pasta: {len(arquivos_txt)}")
+
+arquivos_csv = [f for f in os.listdir(pasta_atual) if f.lower().endswith('.csv')]
+print(f"Quantidade de arquivos .csv na pasta: {len(arquivos_csv)}")
